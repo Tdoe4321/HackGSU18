7cd1aa831a94bacb180fb68f7df5a1a9d4c64de6
Your Name <you@example.com>
2018-10-06 17:52:53 -0400

    Added Simple Debug Crash test

--- button_to_send.ino
+++ button_to_send.ino
@@ -1,14 +1,21 @@
 //pins used for components
 const int buzzer = 4;   // Arduino port pin PD4
+const int crashButton = 5;
+const int resetButton = 6;
 boolean isOn = true;
+boolean didCrash = false;
 unsigned long switchTime = 0;
     
 void setup(){
     pinMode(buzzer, OUTPUT); // set pin for buzzer output
+    pinMode(crashButton, INPUT);
 }
 
 void loop(){
     
+    if(digitalRead(crashButton) == HIGH) didCrash = true;
+    if(digitalRead(resetButton) == HIGH) didCrash = false;
+
     if(millis() - switchTime > 3000) {
         if(isOn) isOn = false;
 		else isOn = true;
