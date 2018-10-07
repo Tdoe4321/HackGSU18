a1317bf31be9a06bfee9eaf14e88029d486bfa3f
Tyler Gragg <tdoe3@comcast.net>
2018-10-07 01:50:04 +0000

    Added LatLon data to python script

--- arduinoLatLon.py
+++ arduinoLatLon.py
@@ -1,7 +1,12 @@
-import serial, time
+import serial, time, geocoder
 ard = serial.Serial('/dev/tty96B0', 9600)
 crash = False
 ardOut = None
+
+
+#import geocoder
+#g = geocoder.ip('me')
+#print(g.latlng)
 
 if __name__ == '__main__':
     print("Welcome to the Crash Report Sensor!")
@@ -13,7 +18,7 @@ if __name__ == '__main__':
                 crash = False
                 ardOut = None
             if(crash == False and ardOut == 1):
-                print(ardOut)
+                print(geocoder.ip('me').latlng)
                 crash = True
                 #time.sleep(1)
             
--- getLatLon.py
+++ getLatLon.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
-import geoclue
-from datetime import datetime
-
-print("Geoclue version %s" % Geoclue.VERSION)
-
-dl = Geoclue.DiscoverLocation()
-dl.init()
-providers = dl.get_available_providers()
-
-for provider in providers:
-  pname = provider['name']
-  dl.set_position_provider(pname)
-  position = dl.get_location_info()
-
-  print("\n%s\n%s" % (pname, "-"*len(pname)))
-  for k, v in position.items():
-    if k.endswith('_timestamp'):
-      v = datetime.fromtimestamp(v)
-    print("%-25s%s" % (k+':',v))
+import geocoder
+g = geocoder.ip('me')
+print(g.latlng)
